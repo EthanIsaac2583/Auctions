0877b72dd4f753ea10f5191d54aedb8ecde79c19
eitan.aizek <eitan.aizek@techork.com>
2024-08-17 17:01:11 +0500

    a: configured ApplicationDbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using Auctions.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auctions.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    public DbSet<Listing> Listings { get; set; }
+    public DbSet<Bid> Bids { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+}
